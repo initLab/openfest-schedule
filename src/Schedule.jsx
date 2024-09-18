77c88b2075eb28b10f247a993f4bf1be3ad18f23
Vencislav Atanasov <git@6bez10.info>
2024-09-18 21:57:36 +0300

    Render all requests data

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -1,10 +1,52 @@
 import PropTypes from 'prop-types';
+import useEvents from './hooks/useEvents.js';
+import useSpeakers from './hooks/useSpeakers.js';
+import useTracks from './hooks/useTracks.js';
+import useEventTypes from './hooks/useEventTypes.js';
+import useHalls from './hooks/useHalls.js';
+import useSlots from './hooks/useSlots.js';
 
 export default function Schedule({
     conferenceId,
 }) {
+    const {
+        data: events,
+    } = useEvents(conferenceId);
+
+    const {
+        data: speakers,
+    } = useSpeakers(conferenceId);
+
+    const {
+        data: tracks,
+    } = useTracks(conferenceId);
+
+    const {
+        data: eventTypes,
+    } = useEventTypes(conferenceId);
+
+    const {
+        data: halls,
+    } = useHalls(conferenceId);
+
+    const {
+        data: slots,
+    } = useSlots(conferenceId);
+
     return (<>
-        conference id: {conferenceId}
+        <div>conference id: {conferenceId}</div>
+        <div>events:</div>
+        <div>{JSON.stringify(events)}</div>
+        <div>speakers:</div>
+        <div>{JSON.stringify(speakers)}</div>
+        <div>tracks:</div>
+        <div>{JSON.stringify(tracks)}</div>
+        <div>event types:</div>
+        <div>{JSON.stringify(eventTypes)}</div>
+        <div>halls:</div>
+        <div>{JSON.stringify(halls)}</div>
+        <div>slots:</div>
+        <div>{JSON.stringify(slots)}</div>
     </>);
 }
 
