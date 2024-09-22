e1ac1959108793028ea7b1e1d8912c43fff710a5
Vencislav Atanasov <git@6bez10.info>
2024-09-23 01:46:32 +0300

    Fix property reading

--- src/Schedule/utils.js
+++ src/Schedule/utils.js
@@ -2,4 +2,4 @@ export const getConferenceYear = conference => (new Date(conference.start_date))
 
 export const getSpeakerName = speaker => speaker.first_name.concat(' ').concat(speaker.last_name);
 
-export const isTrackHidden = track => track.name.en === 'Other' || track.name.bg === 'Други';
+export const isTrackHidden = track => track?.name?.en === 'Other' || track?.name?.bg === 'Други';
