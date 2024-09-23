04cf4740aca8fc54b22ec132fc7f7cad043e80e5
Vencislav Atanasov <git@6bez10.info>
2024-09-24 02:17:37 +0300

    Use concatenated speaker name from API

--- src/Schedule/Speaker.jsx
+++ src/Schedule/Speaker.jsx
@@ -1,16 +1,14 @@
-import { getSpeakerName } from './utils.js';
 import PropTypes from 'prop-types';
 
 export default function Speaker(speaker) {
     return (<>
-        <a href={'#speaker-'.concat(speaker.id.toString())}>{getSpeakerName(speaker)}</a>
+        <a href={'#speaker-'.concat(speaker.id.toString())}>{speaker.name}</a>
         {speaker.organisation && <>/&#8288;{speaker.organisation}&#8288;/</>}
     </>);
 }
 
 Speaker.propTypes = {
     id: PropTypes.number.isRequired,
-    first_name: PropTypes.string.isRequired,
-    last_name: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     organisation: PropTypes.string,
 };
--- src/Schedule/utils.js
+++ src/Schedule/utils.js
@@ -1,3 +1,1 @@
-export const getSpeakerName = speaker => speaker.first_name.concat(' ').concat(speaker.last_name);
-
 export const isTrackHidden = track => track?.name?.en === 'Other' || track?.name?.bg === 'Други';
